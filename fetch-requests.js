7b621a6ccb90e776c2e9693e2a8902fab6d674ce
Brian <brian_@DESKTOP-GJH2BCP>
2023-07-26 20:55:57 -0400

    third commit, phase 3

--- fetch-requests.js
+++ fetch-requests.js
@@ -5,30 +5,53 @@ Paste your code for fetch requests here once you finish each phase.
 
 /* ============================== Phase 1 ============================== */
 
-fetch('/products', {
+fetch('http://localhost:5000/products', {
     method: "POST",
     body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
     headers: {
-        "Content-Type": "application/x-www-urlformencoded"    }
-});
+        "Content-Type": "application/x-www-form-urlencoded"    }
+})
+
 
 
 
 /* ============================== Phase 2 ============================== */
 
-fetch('/products', {
+fetch('http://localhost:5000/products', {
     method: "POST",
     body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
     headers: {
-        "Content-Type": "application/x-www-urlformencoded"    }
+        "Content-Type": "application/x-www-form-urlencoded"    }
 })
-.then((res) => JSON.parse(res))
-.then((res) => console.log(res.statusCode))
-.then((res) => console.log(res.headers['Content-Type']))
-.then((res) => console.log(res.url))
-.then((res) => console.log(res.redirected))
+.then((res) => {
+console.log(res.headers)
+console.log(res.redirected)
+console.log(res.status)
+console.log(res.url)})
+// .then((resData)=> console.log(resData))
+// .then((res) => console.log(res.redirected))
+// .then((res) => console.log(res.statusCode))
+// .then((res) => console.log(res.headers['Content-Type']))
+// .then((res) => console.log(res.url))
+// .then((res) => console.log(res.redirected))
 
+// const res = async function() {
 
+//     let getres = await fetch('http://localhost:5000/products', {
+//        method: "POST",
+//        body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+//        headers: {
+//            "Content-Type": "application/x-www-form-urlencoded"    }
+//    })
+//    // .then((res) => res.text())
+//    .then((getres)=> console.log(getres))
+//    // .then((res) => console.log(res.redirected))
+//    // .then((res) => console.log(res.statusCode))
+//    // .then((res) => console.log(res.headers['Content-Type']))
+//    // .then((res) => console.log(res.url))
+//    // .then((res) => console.log(res.redirected))
+//    }
+//    console.log(res())
 /* ============================== Phase 3 ============================== */
 
 // Your code here
